7bf3137e2916b40d8aade0907d45d1c8e59a9186
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-12-02 10:26:57 -0600

    understood z-indices. Always use bracket operators if someothing isn't working

--- components/test.tsx
+++ components/test.tsx
@@ -3,13 +3,12 @@ import styles from './Test.module.css'
 
 export default function Test() {
   return (
-    <div class={styles.parent}>
-      Parent
-      {/* <img src="wallpapers/L1.jpeg"></img> */}
-      <div class={styles.child_one}>One</div>
-      <div class={styles.child_two}>Two</div>
-      <div class={styles.child_three}>Three</div>
-    </div>
+      <div class="absolute w-full bg-gray-500 p-4">
+        Parent
+        {/* use z-[x] bracket operator for negative AND positive z-index vales.  */}
+        <div class="absolute w-full z-[2] top-1 bg-red-500">One</div>
+        <div class="absolute z-[3] top-[10px] bg-green-400">Two</div>
+      </div>
   )
 }
 
